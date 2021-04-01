a24b35b3fb43c1309cc30cea1f44c3e5eb766a33
Strazdonis <edvinasstrazdonis@gmail.com>
2021-04-01 20:57:38 +0300

    part of verifying email process

--- server/models/user.js
+++ server/models/user.js
@@ -3,17 +3,36 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 //TODO: validation,hashing,everything (handle with passport?)
 
+const validateEmail = (email) => {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
+
 /**
  * main user object
  * username: Strazdonis
  * password: hashed
  * salt: salt user for password hashing
+ * email: edvinasstrazdonis@gmail.com
+ * verified: wether email was verified or not
  * fullname: Edvinas Strazdonis
  * city: Vilnius (detect by IP GEOlocation?)
  */
 const UserSchema = new Schema({
     fullname: { type: String, required: true },
     city: { type: String }, //for weather
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    perms: { type: String, enum: ["normal", "admin"], default: "normal" },
+    code: { type: String },
+    verified: { type: Boolean, default: false }
 });
 
 UserSchema.plugin(passportLocalMongoose);
--- server/routes/api/register.js
+++ server/routes/api/register.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
 const User = require("../../models/user");
 
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 module.exports = (app) => {
     app.post('/register', (req, res) => {
         const data = req.body;
-        if(typeof username !== "string" || typeof data.password !== "string") {
+        if (typeof username !== "string" || typeof data.password !== "string" ) {
             return res.status(400).json({ error: "bad request" });
         }
-        if(data.username.length < 5 || data.password.length < 5) {
+        if (data.username.length < 6 || data.password.length < 6) {
             return res.status(400).json({ error: "username and password must be at least 6 characters long" });
         }
-        User.register({ username: data.username, fullname: `${data.firstname} ${data.lastname}`, city: data.city }, data.password, function (err, user) {
+        const code = makeid(16);
+        User.register({ username: data.username, email: data.email, code: code, fullname: `${data.firstname} ${data.lastname}`, city: data.city }, data.password, function (err, user) {
             if (err) {
                 //TODO: return more reasonable messages & status codes, document them
                 res.status(500).json({ error: err });
